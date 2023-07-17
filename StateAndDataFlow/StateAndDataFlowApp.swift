de13991f68653469a166b288fa91f236f30e37f8
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-07-17 20:33:29 +0400

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Aleksandr Mamlygo on 17.07.23.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
